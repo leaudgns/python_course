d57424cec0936cce3e8a7254c8c1bc6f380215d6
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-03-26 18:00:00 +0900

    python 2.5

--- 2.py
+++ 2.py
@@ -1,18 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
+from indeed import extract_indeed_pages, extract_indeed_jobs
 
-indeed_result = requests.get(
-    "https://kr.indeed.com/%EC%B7%A8%EC%97%85?as_and=%EB%8C%80%EA%B8%B0%EC%97%85&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&as_src=&radius=25&l=&fromage=any&limit=20&sort=&psf=advsrch&from=advancedsearch")
+last_indeed_pages = extract_indeed_pages()
 
-indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
-
-pagination = indeed_soup.find("div", {"class": "pagination"})
-
-links = pagination.find_all('a')
-
-pages = []
-for link in links[:-1]:
-    #pages.append(link.find("span").string)
-    pages.append(int(link.string))
-max_page=pages[-1]
+indeed_jobs = extract_indeed_jobs(last_indeed_pages)
 
