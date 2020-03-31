ff562cc689fafcb84287eee96a5149b29297523a
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-03-31 16:51:20 +0900

    python 3.5 end

--- indeed.py
+++ indeed.py
@@ -2,7 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 
 LIMIT = 20
-URL=f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?l=%EA%B4%91%EC%A3%BC&limit={LIMIT}"
+URL = f"https://kr.indeed.com/%EC%B7%A8%EC%97%85?l=%EA%B4%91%EC%A3%BC&limit={LIMIT}"
+
 
 def get_last_page():
     result = requests.get(URL)
@@ -11,17 +12,18 @@ def get_last_page():
     links = pagination.find_all('a')
     pages = []
     for link in links[:-1]:
-        #pages.append(link.find("span").string)
+        # pages.append(link.find("span").string)
         pages.append(int(link.string))
-    max_page=pages[-1]
+    max_page = pages[-1]
     return max_page
+
 
 def extract_job(html):
     title = html.find("div", {"class": "title"}).find("a")["title"]
     #company = html.find("span",{"class" : "company"}).string
-    company = html.find("span",{"class" : "company"})
+    company = html.find("span", {"class": "company"})
     company_anchor = company.find("a")
-    if company :
+    if company:
         if company_anchor != None:
             company = str(company_anchor.string.strip())
         else:
@@ -30,8 +32,9 @@ def extract_job(html):
         company = None
     # location = str(html.find("span",{"class" : "location"}).string)
     location = html.find("div", {"class": "recJobLoc"})["data-rc-loc"]
+    location = location.strip()
     job_id = html["data-jk"]
-    return {'title':title, 'company': company, 'location': location,'link': f"https://www.indeed.com/rc/clk?jk={job_id}&from=vj&pos=bottom"}
+    return {'title': title, 'company': company, 'location': location, 'link': f"https://www.indeed.com/rc/clk?jk={job_id}&from=vj&pos=bottom"}
 
 
 def extract_jobs(last_page):
@@ -45,6 +48,7 @@ def extract_jobs(last_page):
             job = extract_job(result)
             jobs.append(job)
     return jobs
+
 
 def get_jobs():
     last_page = get_last_page()
